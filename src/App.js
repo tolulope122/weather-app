353fb5ed54216d23237cc4c9fed727b0deb6ddc2
tolulope122 <elizabetholuwafemi07@gmail.com>
2024-08-31 15:41:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+
+  // https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=1d9b0f36e20f99ea00a2a68e7309dd20
+  return (
+    <div className='container'>
+    <div className='top'></div>
+    <div className='bottom'></div>
+  </div>
+  )
+}
